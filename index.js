08d983f0d40a4831c70d59142e7b9d119ddb5cbb
johndlopez <johnlopez@Johns-MacBook-Pro-2.local>
2018-09-06 13:52:57 -0700

    progress on game winning check

--- index.js
+++ index.js
@@ -16,6 +16,32 @@ class Board  {
   printBoard() {
     //print board to console
   }
+  
+  checkForWin() {
+    //Check if X or Y has won
+    let wonCheck = false;
+    for(let y = 0; y < this.board.length; y++) {
+      let count = 0;
+      for(let x = 0; x < this.board[y].length; x++) {
+        if (this.board[y][x] !== '') {
+          count++;
+        }
+      }
+      if (count === 3) {
+        wonCheck = true;
+      }
+    }
+    return wonCheck;
+
+    function checkRow(index) {
+      //check space values for a match
+      
+    }
+
+    function checkSpace(index) {
+
+    }
+  }
 
   placeX(x, y) {
     if (spaceIsEmpty) {
@@ -23,6 +49,7 @@ class Board  {
     } else {
       console.log('This space was already taken :(')
     }
+  checkForWin();
   }
 
   placeO(x,y) {
@@ -31,6 +58,7 @@ class Board  {
     } else {
       console.log('This space was already taken :(')
     }
+    checkForWin();
   }
 
   spaceIsEmpty(x,y) {
